5a263f9b36d9f38fedea5b23ec4f610cc01d4f19
kdharani242-source <kdharani242@gmail.com>
2026-06-07 05:29:29 +0530

    Create 513)Janmansh and Assignments.c

---
+++ 513)Janmansh and Assignments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	  int x;
+	  scanf("%d",&x);
+	  if((10-x)>=3){
+	      printf("yes\n");
+	  }
+	  else{
+	      printf("no\n");
+	  }
+	}
+
+}
