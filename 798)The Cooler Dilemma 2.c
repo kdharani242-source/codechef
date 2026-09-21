a3716cca5700f4207371a5436b50031acc81886b
kdharani242-source <kdharani242@gmail.com>
2026-09-21 18:48:38 +0530

    Create 798)The Cooler Dilemma 2.c

---
+++ 798)The Cooler Dilemma 2.c
@@ -0,0 +1,26 @@
+The summer is at its peak in Chefland. Chef is planning to purchase a water cooler to keep his room cool. He has two options available:
+
+Rent a cooler at the cost of 
+X
+X coins per month.
+Purchase a cooler for 
+Y
+Y coins.
+Chef wonders what is the maximum number of months for which he can rent the cooler such that the cost of renting is strictly less than the cost of purchasing it.
+
+
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    printf("%d\n",(y-1)/x);
+	}
+
+}
+
